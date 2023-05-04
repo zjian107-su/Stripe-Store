d4605302216da7b60c2633ecb4088b0923b37ab9
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-05-03 23:23:07 -0400

    ✨ update: create card view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from "./pages/home/home.component";
 import { ProductsHeaderComponent } from "./pages/home/components/products-header/products-header.component";
 import { FiltersComponent } from "./pages/home/components/filters/filters.component";
+import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { FiltersComponent } from "./pages/home/components/filters/filters.compon
     HomeComponent,
     ProductsHeaderComponent,
     FiltersComponent,
+    ProductBoxComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component } from "@angular/core";
 
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
 })
 export class HomeComponent {
-  col = 3;
+  cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
   category = "milk";
 
   onColumnsCountChange(colsNum: number): void {
-    this.col = colsNum;
+    this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
   onShowCategory(newCategory: string): void {
