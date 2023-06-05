32db94fa1403e110aa79e1a7c6450d9983feb575
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-06-05 17:32:45 -0400

    ✨ update: config stripe for checkout within the front-end

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { Cart, CartItem } from "../../models/cart.model";
 import { CartService } from "src/app/services/cart.service";
 import { Subscription } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { loadStripe } from "@stripe/stripe-js";
 
 @Component({
   selector: "app-cart",
@@ -25,7 +27,7 @@ export class CartComponent implements OnInit {
     "action",
   ];
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private http: HttpClient) {}
   ngOnInit(): void {
     // this.dataSource = this.cart.items;
     this.cartSubscription = this.cartService.cart.subscribe((_cart: Cart) => {
@@ -52,5 +54,20 @@ export class CartComponent implements OnInit {
 
   onRemoveQuantity(item: CartItem): void {
     this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http
+      .post("http://localhost:4200/checkout", {
+        items: this.cart.items,
+      })
+      .subscribe(async (res: any) => {
+        let stripe = await loadStripe(
+          "pk_live_51NHCJbE6UXM1NPAkrLQJTNTahAb1uoYdiRnvLIX2qbgb2nnXMMVAff3wIWYRKVFbDh5OuN4FSxSkhAbb4Y34XfXL00aE5SR6Aj"
+        );
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      });
   }
 }
